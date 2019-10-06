76f15209019def2328ab3dfc8f30d37a1303069b
kyubuns <kyubuns@gmail.com>
2019-10-07 00:57:40 +0900

    feat: unitypackage作るために整理

---
+++ Assets/PackageExporter.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public static class PackageExporter
+{
+    [MenuItem("Export/Export Unity Package")]
+    public static void ExportUnityPackage()
+    {
+        var libraryDirectories = new[]
+        {
+            "Assets/PaletteSwap"
+        };
+
+        var outputFilePath = Path.Combine(Path.GetDirectoryName(Application.dataPath), "PaletteSwap.unitypackage");
+
+        Debug.LogFormat("Export package to {0}", outputFilePath);
+
+        AssetDatabase.ExportPackage(libraryDirectories, outputFilePath, ExportPackageOptions.Recurse);
+
+        Debug.LogFormat("Success");
+    }
+}
